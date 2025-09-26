b49358fed7b19197b4c81a290fce4964474f93a2
Filipe borges <filipe_mborges@live.com>
2025-09-25 22:50:43 -0300

    feat: adicionados o GET, DELETE e login

--- src/main/java/com/Javax/usuario/business/UsuarioService.java
+++ src/main/java/com/Javax/usuario/business/UsuarioService.java
@@ -4,6 +4,7 @@ import com.Javax.usuario.business.converter.UsuarioConverter;
 import com.Javax.usuario.business.dto.UsuarioDTO;
 import com.Javax.usuario.infrastructure.entity.Usuario;
 import com.Javax.usuario.infrastructure.exceptions.ConflictException;
+import com.Javax.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.Javax.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -39,5 +40,13 @@ public class UsuarioService {
 
     public boolean verificaEmailExistente(String email){
         return usuarioRepository.existsByEmail(email);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado" + email));
+    }
+    public void deletaUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
     }
 }
